8555abb0fb5732f4202114f94fd3f3bc4d26b879
bmsrangel <bmsrangel@hotmail.com>
2021-11-14 18:30:26 -0300

    Implemented creation, deletion and update methods for orders repository

--- lib/app/core/data/orders/i_orders_repository.dart
+++ lib/app/core/data/orders/i_orders_repository.dart
@@ -1,5 +1,9 @@
+import 'package:quiosque/app/core/data/dtos/order_dto.dart';
 import 'package:quiosque/app/core/models/order_model.dart';
 
 abstract class IOrdersRepository {
   Future<List<OrderModel>> getAllOrders();
+  Future<int> createOrder(OrderDTO orderDTO);
+  Future<int> updateOrder(OrderDTO updatedOrder);
+  Future<int> deleteOrder(int orderId);
 }
